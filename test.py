c06956bee0aaf0938308d62038ba66cccb7b2545
Jay <96758146+JamesDarf@users.noreply.github.com>
2023-08-07 12:01:03 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,15 @@
+import os
+import openai
+
+openai.api_key = "ur key"
+
+completion = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "user", "content": "Hello!"}
+  ]
+)
+
+print(completion.choices[0].message)
+
+
